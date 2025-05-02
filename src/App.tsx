d492626ecf023a1eea258aadaf5ab9a4f27d682e
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-05-01 22:04:49 -0700

    switched around the file structure

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { HandwritingText } from 'react-cursive-handwrite';
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>React Cursive Handwriting Demo</h1>
+      <HandwritingText 
+        fontPath="./fonts/google"
+        strokeColor="#FFFFFF"
+        strokeWidth={2}
+        duration={8}
+      >
+        {'Joshua Goodman'}
+      </HandwritingText>
+    </div>
+  )
+}
+
+export default App
